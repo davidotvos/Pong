3a27cca68921be9f1308729dabce172c18392eff
David <61760404+davidotvos@users.noreply.github.com>
2023-05-27 20:47:59 +0200

    Moving the paddles, drawing middle line

--- main.py
+++ main.py
@@ -43,16 +43,40 @@ def draw(win, paddles):
     win.fill(BLACK)
     pygame.display.update()
 
+    # Drawing the paddles
     for paddle in paddles:
         paddle.draw(win)
+
+
+    # Middle separation line
+    for i in range(10, HEIGHT, HEIGHT//20):
+        if i % 2 == 1:
+            continue
+        pygame.draw.rect(win, WHITE, (WIDTH//2 - 5, i, 10, HEIGHT//20))
+
 
     pygame.display.update()
 
 
-def main():
-    run = True
-    clock = pygame.time.Clock()
+def handle_paddle_movement(keys, left_paddle, right_paddle):
+    # Handle movement for the left paddle
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
+        left_paddle.move(up=True)  # Move the left paddle up if 'W' key is pressed and paddle won't go off the screen
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
+        left_paddle.move(up=False)  # Move the left paddle down if 'S' key is pressed and paddle won't go off the screen
 
+    # Handle movement for the right paddle
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
+        right_paddle.move(up=True)  # Move the right paddle up if 'UP' arrow key is pressed and paddle won't go off the screen
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
+        right_paddle.move(up=False)  # Move the right paddle down if 'DOWN' arrow key is pressed and paddle won't go off the screen
+
+
+def main():
+    run = True  # Variable to control the game loop
+    clock = pygame.time.Clock()  # Create a clock object to control the frame rate
+
+    # Create left and right paddles with initial positions
     left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
 
@@ -64,6 +88,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
 
     pygame.quit()
 
